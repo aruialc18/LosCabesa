79529cd42ee18cf3852120fda86c09178bc40e86
cristobalguillen47 <cguibar09@iesmarquesdecomares.org>
2021-11-18 12:20:24 +0100

    cambios

--- truenti/framework/includes/footer.php
+++ truenti/framework/includes/footer.php
@@ -3,19 +3,18 @@
             <div class="row">
                 <!-- Footer Icono-->
                 <div class="col-lg-4 mb-5 mb-lg-0">
-                <img  class="logo" src="assets/img/truenti/logo.png"  />
-                   
+                    <img  class="logo" src="assets/truenti/logo.png"  />
                 </div>
                 <!-- Footer Quienes Somos-->
-                <div class="col-lg-4 mb-5 mb-lg-0">
-                <a href="">¿Quienes somos?</a>
-                </div>
-                <!-- Footer Unete-->
-                <div class="col-lg-4">
-                 
+                
+                    <div  class="col-lg-4 mb-5 mb-lg-0 prueba">
+                        <a href="">¿Quienes somos?</a>
+                    </div>
+                    <!-- Footer Unete-->
+                    <div class="col-lg-4">
                         <a href="">Únete</a>
-                   
-                </div>
+                    </div>
+                
             </div>
         </div>
     </footer>
