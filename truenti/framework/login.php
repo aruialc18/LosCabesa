2f3049b8f914e4725e72c71678b21b3c94566201
Ablarom28 <ablarom28@iesmarquesdecomares.org>
2021-11-18 12:51:58 +0100

    hdueksfb

--- truenti/framework/login.php
+++ truenti/framework/login.php
@@ -5,12 +5,12 @@
 </head>
 <body>
     
-            <div class="container-fluid" style="background-image: url(/assets/truenti/fondo2.png)">
+            <div class="container-fluid page-section portfolio" style="background-image: url(/assets/truenti/fondo2.png)">
                 <div class="row justify-content-center">
-                     <div class="col-md-6">
-                        <img class="logo-login" src="/assets/truenti/logo.png">
+                     <div class="col-md-6 h-100 text-center">
+                        <img class="logo-login" src="/assets/truenti/rayo2.png">
                      </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6" style="padding: 10px">
                         <form>
                             <input type="text" name="user" placeholder="Usuario"><br><br>
                             <input type="password" name="pass" placeholder="Contraseña"><br><br>
