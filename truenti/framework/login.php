50992f385c9e22c201da50854209d07510925ae1
Ablarom28 <ablarom28@iesmarquesdecomares.org>
2021-11-18 11:30:27 +0100

    add login

--- truenti/framework/login.php
+++ truenti/framework/login.php
@@ -4,20 +4,22 @@
 <?php include("includes/head-tag-contents.php");?>
 </head>
 <body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-6">
-                <img 
-            </div>
-            <div class="col-md-6">
-
-            </div>
-        </div>
-
-
-    </div>
-
-
+    
+            <div class="container-fluid" style="background-image: url(/assets/truenti/fondo2.png)">
+                <div class="row justify-content-center">
+                     <div class="col-md-6">
+                        <img class="logo-login" src="/assets/truenti/logo.png">
+                     </div>
+                    <div class="col-md-6">
+                        <form>
+                            <input type="text" name="user" placeholder="Usuario"><br><br>
+                            <input type="password" name="pass" placeholder="Contraseña"><br><br>
+                            <input class="bg-secondary" type="submit" name="entrar" value="Entrar">
+                         </form>
+                     </div>
+                </div>
+             </div>
+     
 
 <?php include("includes/footer.php");?>
 </body>
