577407d26a62b56af4b1df110acdb0ba04c41b22
plopbon30 <plopbon30@iesmarquesdecomares.org>
2021-11-19 08:40:20 +0100

    Navbar y logo3.png

--- truenti/framework/includes/navigation.php
+++ truenti/framework/includes/navigation.php
@@ -1,36 +1,31 @@
 <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-    <div class="container">
-        <a class="navbar-brand" href="index.php"></a>
-        <button class="navbar-toggler text-uppercase font-weight-bold text-white rounded" type="button"
-            data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
-            aria-expanded="false" aria-label="Toggle navigation">
-            Menu
-            <i class="fas fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
+    
+        <a class="navbar" href="index.php"><img src="assets/truenti/logo3.png" width="40%" height="50%"></a>
+        
+        <div class="collapse navbar-collapse d-flex" id="navbarResponsive">
 
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link py-3 px-0 px-lg-3  dropdown-toggle" href="#" id="navbarDropdown" role="button"
+            <ul class="navbar-nav d-flex justify-content-around flex-grow-1">
+                <li class="nav-item">
+                    <a class="nav-link py-3 px-0 px-lg-3"  dropdown-toggle href="#" id="navbarDropdown" role="button"
                         data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
+                        Inicio
                     </a>
-                    <div class="dropdown-menu bg-secondary text-uppercase border-0 " aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item text-white" href="#">Action</a>
-                        <a class="dropdown-item text-white" href="#">Another action</a>
-                        <a class="dropdown-item text-white" href="#">Something else here</a>
-                    </div>
                 </li>
-                <li class="nav-item mx-0 mx-lg-1"><a
-                        class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?> py-3 px-0 px-lg-3 rounded"
-                        href="index.php">Portfolio</a></li>
-                <li class="nav-item mx-0 mx-lg-1"><a
-                        class="nav-link <?php if ($CURRENT_PAGE == "About") {?>active<?php }?> py-3 px-0 px-lg-3 rounded"
-                        href="about.php">About</a></li>
-                <li class="nav-item mx-0 mx-lg-1"><a
-                        class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?> py-3 px-0 px-lg-3 rounded"
-                        href="contact.php">Contact</a></li>
+                <li class="nav-item"><a
+                        class="nav-link py-3 px-0 px-lg-3 rounded"
+                        href="about.php">Perfil</a></li>
+
+                <li class="nav-item"><a class="nav-link py-3 px-0 px-lg-3 rounded" 
+                        href="contact.php">¿Quienes somos?</a></li>
+
+                <li class="nav-item"><a
+                        class="nav-link py-3 px-0 px-lg-3 rounded"
+                        href="contact.php">Compra/venta</a></li>
+
+                <li class="nav-item"><a
+                        class="nav-link py-3 px-0 px-lg-3 rounded"
+                        href="index.php">Cerrar Sesion</a></li>
             </ul>
         </div>
-    </div>
+  
 </nav>
