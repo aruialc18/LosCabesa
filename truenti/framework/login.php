9b0b1b897c42a09f58d3be510bff630cfc7c0315
Ablarom28 <ablarom28@iesmarquesdecomares.org>
2021-11-21 11:41:44 +0100

    add

--- truenti/framework/login.php
+++ truenti/framework/login.php
@@ -5,25 +5,26 @@
 </head>
 <body>
     
-            <div class="container-fluid page-section portfolio" style="background-image: url(/assets/truenti/fondo2.png);    " >
-                <div class="row justify-content-center">
-                     <div class="col-sm-12 col-md-6 col-lg-6 h-100 text-center">
-                        <img class="col-sm-12 logo-login" src="/assets/truenti/rayo2.png">
+            <div class="container-fluid page-section " style="background-image: url(/assets/truenti/fondo2.png); " >
+                <div class="row">
+                     <div class="col-sm-12 col-md-6">
+                        <img class=" logo-login" src="/assets/truenti/rayo2.png">
                      </div>
-                    <div class="col-sm-12 col-md-6 col-lg-6 h-100 text-center" >
+                    <div class="col-sm-12  col-lg-6">
                        <h1>Inicio de Sesión</h1>
                         <form class="form-login">
                             <input type="text" name="user" placeholder="Usuario"><br><br>
                             <input type="password" name="pass" placeholder="Contraseña"><br><br>
-                            <input class="bg-secondary" type="submit" name="entrar" value="Entrar">
+                            <input class="bg-secondary" type="submit" name="entrar" value="Entrar"><br>
+                            <h>¿Aún no tienes cuenta? <a href="register.php">Regístrate</a></h2>
                          </form>
-                         <h>¿Aún no tienes cuenta? <a href="register.php">Regístrate</a></h2>
+                         
                      </div>
                      
                 </div>
              </div>
              <?php include("includes/footer.php");?>
 
-
+             
 </body>
 </html>
