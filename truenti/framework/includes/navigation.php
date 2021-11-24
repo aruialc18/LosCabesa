74cf0ab52bfba5e7be34f5126527322c46609a4d
plopbon30 <plopbon30@iesmarquesdecomares.org>
2021-11-24 13:32:11 +0100

    sddssdds

--- truenti/framework/includes/navigation.php
+++ truenti/framework/includes/navigation.php
@@ -1,3 +1,16 @@
+<div class="collapse" id="navbarToggleExternalContent">
+  <div class="bg-dark p-4">
+    <h5 class="text-white h4">Collapsed content</h5>
+    <span class="text-muted">Toggleable via the navbar brand.</span>
+  </div>
+</div>
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
+</nav>
 <nav class="navbar navbar-expand-lg bg-black text-uppercase fixed-top" id="mainNav">
     
         <a href="index.php"><img class="logo" src="assets/truenti/logo3.png" width="40%" height="50%"></a>
@@ -5,25 +18,19 @@
         <div class="collapse navbar-collapse d-flex" id="navbarResponsive">
 
             <ul class="navbar-nav d-flex justify-content-around flex-grow-1">
-                <li class="nav-item">
-                    <a class="nav-link py-3 px-0 px-lg-3" href="#" role="button">
-                        Inicio
-                    </a>
-                </li>
-                <li class="nav-item"><a
-                        class="nav-link py-3 px-0 px-lg-3 rounded"
+                <li class="nav-item"><a class="nav-link py-3 px-0 px-lg-3" href="#">Inicio</a></li>
+                <li class="nav-item"><a class="nav-link py-3 px-0 px-lg-3"
                         href="about.php">Perfil</a></li>
 
-                <li class="nav-item"><a class="nav-link py-3 px-0 px-lg-3 rounded" 
+                <li class="nav-item"><a class="nav-link py-3 px-0 px-lg-3" 
                         href="contact.php">¿Quienes somos?</a></li>
 
-                <li class="nav-item"><a
-                        class="nav-link py-3 px-0 px-lg-3 rounded"
+                <li class="nav-item"><a class="nav-link py-3 px-0 px-lg-3"
                         href="contact.php">Compra/venta</a></li>
 
-                <li class="nav-item"><a
-                        class="nav-link py-3 px-0 px-lg-3 rounded"
-                        href="index.php"><button type="button" class="btn btn-primary" >Cerrar Sesion</button></a></li>
+                <li class="nav-item"><a class="nav-link py-3 px-0 px-lg-3"
+                        href="index.php">
+                        <button type="button" class="btn btn-primary">Cerrar Sesion</button></a></li>
             </ul>
         </div>
   
