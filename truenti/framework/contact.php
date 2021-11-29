5ede7be04b134a5665acf27202a40e5441cd76c2
aruialc18 <91251985+aruialc18@users.noreply.github.com>
2021-11-29 13:03:06 +0100

    sss

--- truenti/framework/contact.php
+++ truenti/framework/contact.php
@@ -12,11 +12,11 @@
 <section class="contact" id="contact">
         <div class="container">
             <!-- Contact Section Heading-->
-            <h2 class="page-section-heading text-uppercase  mb-0 text-align: left">Acerca de nosotros</h2>
+            <h2 class="page-section-heading text-uppercase  mb-0 text-align: left">Acerca de nosotros</h2><br>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
            
             <br><br><br>
-            <h2 class="page-section-heading text-uppercase  mb-0 text-align: left">Servicio de soporte y atención al cliente</h2>
+            <h2 class="page-section-heading text-uppercase  mb-0 text-align: left">Servicio de soporte y atención al cliente</h2><br>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
             <br><br><br>
@@ -86,7 +86,8 @@
                             <div class="text-center text-danger mb-3">Error sending message!</div>
                         </div>
                         <!-- Submit Button-->
-                        <button class="btn btn-primary btn-xl disabled" id="submitButton" type="submit">Send</button>
+                        <button type="button" class="btn btn-primary"> Enviar</button>
+                        <br><br>
                     </form>
                 </div>
             </div>
