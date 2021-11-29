8e8d381405551ca3c758c5b0f37a6daac40529ef
Ablarom28 <ablarom28@iesmarquesdecomares.org>
2021-11-29 13:09:44 +0100

    add

--- truenti/framework/product.php
+++ truenti/framework/product.php
@@ -8,7 +8,24 @@
 
 <?php include("includes/navigation.php");?>
 <main>
-    
+    <section class="page-product" id="product">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="dropdown text-center">
+                    <button type="button" class="btn btn-white dropdown-toggle " data-bs-toggle="dropdown" style="border: 1px solid">
+                        Categoría
+                     </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Servicios</a></li>
+                        <li><a class="dropdown-item" href="#">Productos</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-md-4 tarjetas">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 
