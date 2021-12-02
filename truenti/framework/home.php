7cad28bba5f2a0ec0008cf6dbf68ab3792513ac7
cristobalguillen47 <cguibar09@iesmarquesdecomares.org>
2021-12-02 12:24:48 +0100

    home

--- truenti/framework/home.php
+++ truenti/framework/home.php
@@ -6,30 +6,45 @@
 </head>
 <body>
 <?php include("includes/navigation.php");?>
- <div class="container-fluid separatop d-flex" >
-   <div class="row">
-     <div class="justify-content-center text-center">
-     
-        <form>
-          <img class="imguser" src="assets/truenti/user.jpg" />
-          <textarea rows="4" cols="80" placeholder="Escribe aqui tus progresos..."></textarea>
-        </form>
-      
-      <div>
-        
-     </div>
-    <div>
+ <form> 
+   <div class="container-fluid separatop " >
+ 
+ <div class="row">
+    <div class="col-2"> 
 
+          <img class="img-fluid imguser" src="assets/truenti/user.jpg" />
     </div>
+    <div class="col-10 d-flex align-items-center"> 
+          <textarea rows="4" cols="130" placeholder="Escribe aqui tus progresos..."></textarea>
+    </div>       
+  </div>
+  <div class="lineasepara" > </div>
 
+  <div class="row separatop">
+    <div class="col-2"> 
 
-   
+          <img class="img-fluid imguser" src="assets/truenti/user.jpg" />
+    </div>
+    <div class="col-10 d-flex align-items-center"> 
+          <textarea rows="4" cols="130" placeholder="Escribe aqui tus progresos..."></textarea>
+    </div>       
+  </div>
+<br>
+<div class="lineasepara" > </div>
+  <div class="row separatop">
+    <div class="col-2"> 
+
+          <img class="img-fluid imguser" src="assets/truenti/user.jpg" />
+    </div>
+    <div class="col-10 d-flex align-items-center"> 
+          <textarea rows="4" cols="130" placeholder="Escribe aqui tus progresos..."></textarea>
+    </div>       
   </div>
 
   
 </div>
 
-
+</form>
 
 </body>
 </html>
