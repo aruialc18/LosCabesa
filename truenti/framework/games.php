5a603985ab1f48a94e52db35f78e1d2095b9e339
Ablarom28 <ablarom28@iesmarquesdecomares.org>
2022-01-13 12:37:09 +0100

    up games

--- truenti/framework/games.php
+++ truenti/framework/games.php
@@ -13,14 +13,43 @@
     <h1>Truenti Games</h1>
     </div>
     <div class="container-fluid separatop">
-    <div class="container col-md-4">
-                    <h2>Cristobal's Game</h2>
-                    <div class="card" style="width:400px">
-                        <img class="card-img-top" src="../assets/productos/proteina.jpg" alt="Card image" style="width:100%">
-                             <div class="card-body">
-                                <a href="./juegos/cristobal/index.html" class="btn btn-primary">Jugar</a>
-                            </div>
-                    </div>      
-    <a href="">Juego 1</a>
+        <div class="row">
+            <div class="col-md-4 text-center">
+                <div class="card" style="width:400px">
+                    <h2>María Sist</h2>
+                    <img class="card-img-top" src="../assets/recursosjuegos/miniaturacristobal.png" alt="Card image" style="width:100%">
+                    <div class="card-body">
+                        <a href="./juegos/cristobal/index.html" class="btn btn-primary">Jugar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="card" style="width:400px">
+                    <h2>Juego Pablo</h2>
+                    <img class="card-img-top" src="../assets/recursosjuegos/miniaturapablo.png" alt="Card image" style="width:100%">
+                    <div class="card-body">
+                        <a href="./juegos/pablo/index.html" class="btn btn-primary">Jugar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="card" style="width:400px">
+                    <h2>Juego Antonio</h2>
+                    <img class="card-img-top" src="../assets/recursosjuegos/miniaturaantonio.png" alt="Card image" style="width:100%">
+                    <div class="card-body">
+                        <a href="./juegos/antonio/index.html" class="btn btn-primary">Jugar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="card" style="width:400px">
+                    <h2>Juego Adri</h2>
+                    <img class="card-img-top" src="../assets/recursosjuegos/miniaturaadri.png" alt="Card image" style="width:100%">
+                    <div class="card-body">
+                        <a href="./juegos/adri/index.html" class="btn btn-primary">Jugar</a>
+                    </div>
+                </div>
+            </div>
+        </div>     
     </div>
 </div>
