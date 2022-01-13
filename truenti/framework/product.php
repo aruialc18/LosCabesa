fe1e42c83268ac61592f0b81e894c1202b8999b8
Antonio Ruiz-Ruano <91251985+aruialc18@users.noreply.github.com>
2022-01-13 14:24:07 +0100

    SD
    GO

--- truenti/framework/product.php
+++ truenti/framework/product.php
@@ -26,10 +26,10 @@
                 <div class="container col-md-4">
                     <h2>Impact Whey Protein</h2>
                     <div class="card" style="width:400px">
-                        <img class="card-img-top" src="../assets/productos/proteina.jpg" alt="Card image" style="width:100%">
+                        <img class="card-img-top" src="../assets/productos/peanutbutterb.jpg" alt="Card image" style="width:100%">
                              <div class="card-body">
-                                <h4 class="card-title">Proteína de suero de leche</h4>
-                                <p class="card-text">Este suero de alta calidad incluye 21 g de proteína por ración, toda la proteína que necesitas de una fuente de calidad: las mismas vacas que producen tu leche y queso.</p>
+                                <h4 class="card-title">Mantequilla de Cacahuete Natural</h4>
+                                <p class="card-text">Nuestra Mantequilla de Cacahuete Natural es un alimento en forma de crema para untar, completamente natural y muy versátil. Está elaborada de forma natural con cacahuetes tostados y no contiene sal, azúcar, aceite de palma ni conservantes añadidos, siendo una forma dulce y sencilla de reforzar tus entrenamientos.</p>
                                  <h4 class="card-title">19,99€</h4>
                                 <a href="#" class="btn btn-primary">Compra rápida</a>
                             </div>
@@ -40,8 +40,8 @@
                     <div class="card" style="width:400px">
                         <img class="card-img-top" src="../assets/productos/proteina.jpg" alt="Card image" style="width:100%">
                              <div class="card-body">
-                                <h4 class="card-title">Proteína de suero de leche</h4>
-                                <p class="card-text">Este suero de alta calidad incluye 21 g de proteína por ración, toda la proteína que necesitas de una fuente de calidad: las mismas vacas que producen tu leche y queso.</p>
+                                <h4 class="card-title">BCAA 2:1:1</h4>
+                                <p class="card-text">Una mezcla para el día a día de los aminoácidos esenciales (leucina, isoleucina y valina) que se encuentran presentes de forma natural en las proteínas y que ayudan a aumentar y regenerar la musculatura. De este modo, podrás asegurarte de que tu cuerpo estará preparado para afrontar la siguiente sesión de entrenamiento.</p>
                                  <h4 class="card-title">19,99€</h4>
                                 <a href="#" class="btn btn-primary">Compra rápida</a>
                             </div>
@@ -52,8 +52,8 @@
                     <div class="card" style="width:400px">
                         <img class="card-img-top" src="../assets/productos/proteina.jpg" alt="Card image" style="width:100%">
                              <div class="card-body">
-                                <h4 class="card-title">Proteína de suero de leche</h4>
-                                <p class="card-text">Este suero de alta calidad incluye 21 g de proteína por ración, toda la proteína que necesitas de una fuente de calidad: las mismas vacas que producen tu leche y queso.</p>
+                                <h4 class="card-title">Mezcla Ganador de Peso</h4>
+                                <p class="card-text">Este gran suplemento cuenta con 31 g de proteína, unos alucinantes 50 g de carbohidratos, y un total de 388 calorías por ración, poniéndote en marcha para que consigas esos avances tan importantes1, todo mientras potencia tu recuperación tras los entrenamientos de alta intensidad.</p>
                                  <h4 class="card-title">19,99€</h4>
                                 <a href="#" class="btn btn-primary">Compra rápida</a>
                             </div>
