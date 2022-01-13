e167441aa72dab9619d6d87a597d238259fead59
Ablarom28 <ablarom28@iesmarquesdecomares.org>
2022-01-13 11:22:54 +0100

    update games

--- truenti/framework/games.php
+++ truenti/framework/games.php
@@ -11,8 +11,16 @@
 <div class="container-fluid separatop">
     <div class="page-juegos">
     <h1>Truenti Games</h1>
-
-
-
+    </div>
+    <div class="container-fluid separatop">
+    <div class="container col-md-4">
+                    <h2>Cristobal's Game</h2>
+                    <div class="card" style="width:400px">
+                        <img class="card-img-top" src="../assets/productos/proteina.jpg" alt="Card image" style="width:100%">
+                             <div class="card-body">
+                                <a href="./juegos/cristobal/index.html" class="btn btn-primary">Jugar</a>
+                            </div>
+                    </div>      
+    <a href="">Juego 1</a>
     </div>
 </div>
