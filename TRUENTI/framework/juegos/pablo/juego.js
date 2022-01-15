2e8068d4acead9c0c2b6121f45602c920dfc970a
plopbon30 <plopbon30@iesmarquesdecomares.org>
2022-01-15 19:43:19 +0100

    JuegoPablo

---
+++ TRUENTI/framework/juegos/pablo/juego.js
@@ -0,0 +1,50 @@
+String.prototype.replaceAt = function (index, character) { return this.substr(0, index) + character + this.substr(index + character.length); }
+//Uso: palabraConGuiones = palabraConGuiones.replaceAt(i*2, letra);
+
+
+//Array de palabras que van a salir aleatoriamente
+const palabras = ['casa', 'perro', 'gato', 'elefante'];
+
+//Esto eligirá una palabra aleatoria del array de palabras
+const palabra = palabras[Math.floor(Math.random() * palabras.length)];
+
+//reemplazamos la palabra aleatoria por guiones
+let palabraConGuiones = palabra.replace(/./g, "_ ");
+
+
+let contadorFallos = 0;
+
+//pintamos los guiones
+document.querySelector('#output').innerHTML = palabraConGuiones;
+
+document.querySelector('#calcular').addEventListener('click', () => {
+    const letra = document.querySelector('#letra').value;
+
+    let hafallado = true;
+
+    //la i va a recorrer la palabra aleatoria y si la letra coincide con alguna letra de las palabras con guiones sustituye el guion por la letra
+    for (const i in palabra) {
+        if (letra == palabra[i]) {
+            palabraConGuiones = palabraConGuiones.replaceAt(i * 2, letra); //El *2 es debido a que en la variable palabraConGuiones tenemos el guion y un espacio en blanco y cuenta como 2 caracteres
+            hafallado = false;
+        }
+    }
+
+    if (hafallado) {
+        contadorFallos++;
+        document.querySelector('#ahorcado').style
+            .backgroundPosition = -(307 * contadorFallos) + 'px 0';
+        if (contadorFallos == 4) {
+            alert("casi")
+        }
+    }else{
+        if(palabraConGuiones.indexOf('_') < 0){
+            alert("has ganado")
+        }
+    }
+
+    document.querySelector('#output').innerHTML = palabraConGuiones;
+
+    document.querySelector('#letra').value = '';
+    document.querySelector('#letra').focus();
+});
