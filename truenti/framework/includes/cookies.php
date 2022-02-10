df4bedb5f49e9a6f24b9314aabb101a5a5f93929
Ablarom28 <ablarom28@iesmarquesdecomares.org>
2022-02-10 10:38:53 +0100

    add

--- truenti/framework/includes/cookies.php
+++ truenti/framework/includes/cookies.php
@@ -6,7 +6,7 @@ $advice = "<div style='text-align:center;top:0;position:fixed;width:100%;
 
 z-index:1000;background-color:#F374A9;'>
 
-TruentiSport hace uso de la gestión de cookies para ofrecerte una mejor experiencia. <a href='' onclick='hide()'>[ Acepto ]</a>
+TruentiSport© hace uso de la gestión de cookies para ofrecerte una mejor experiencia. <a href='' onclick='hide()'>[ Acepto ]</a>
 
 </div>";
 
