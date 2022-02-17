1ff3ce6a8a8e718b8ee2bd57238883a6db8c789e
Antonio Ruiz-Ruano <91251985+aruialc18@users.noreply.github.com>
2022-02-17 12:49:04 +0100

    registro siuuuuuuuuu

--- truenti/framework/registeruser.php
+++ truenti/framework/registeruser.php
@@ -19,28 +19,15 @@ if ($conn ->connect_error) {
       if(isset($_POST['enviar'])) { 
          if($_POST['usuario'] == '' or $_POST['password'] == '' or $_POST['correo'] == '') { 
              echo 'Por favor llene todos los campos.'; 
-         } else { 
-             $sql = 'SELECT * FROM usuario'; 
-             $rec = mysql_query($sql); 
-             $verificar_usuario = 0; 
-             while($result = mysql_fetch_object($rec)) { 
-                 if($result->usuario == $_POST['usuario']) { 
-                     $verificar_usuario = 1; 
-                 } 
-             } 
-             if($verificar_usuario) { 
+             }  
                      $usuario = $_POST['usuario']; 
                      $password = $_POST['password']; 
                      $correo = $_POST['correo'];
                      $sql = "INSERT INTO usuario (username,password,correo_electronico) VALUES ('$usuario','$password', '$correo')"; 
-                     mysql_query($sql); 
+                     $conn->query($sql);
                      header("Location: index.php");
                  } 
-              else {
-                 echo 'Este usuario ya ha sido registrado anteriormente.'; 
-             } 
-         } 
-     } 
+              
 
 
       ?>
