f66bbb51af204701619effb11fb6ccf27298ccfd
Antonio Ruiz-Ruano <91251985+aruialc18@users.noreply.github.com>
2022-02-17 11:14:38 +0100

    gg

--- truenti/framework/register.php
+++ truenti/framework/register.php
@@ -13,11 +13,11 @@
                      </div>
                     <div class="col-sm-12  col-md-6">
                        <h1>Registro</h1>
-                        <form class="form-login">
-                            <input type="text" name="user" class="form-control-sm" placeholder="Usuario"><br><br>
-                            <input type="text" name="user" class="form-control-sm" placeholder="Correo Electrónico"><br><br>
-                            <input type="password" name="pass" class="form-control-sm" placeholder="Contraseña"><br><br>
-                            <input class="bg-secondary form-control-sm" type="submit" name="entrar" value="Crear Cuenta"><br><br>
+                        <form action="registeruser.php" method="POST" class="form-login">
+                            <input type="text" name="usuario" class="form-control-sm" placeholder="Usuario"><br><br>
+                            <input type="text" name="correo" class="form-control-sm" placeholder="Correo Electrónico"><br><br>
+                            <input type="password" name="password" class="form-control-sm" placeholder="Contraseña"><br><br>
+                            <input class="bg-secondary form-control-sm" type="submit" name="enviar" value="Crear Cuenta"><br><br>
                             <h>¿Ya tienes cuenta? <a href="index.php">Inicia Sesión</a></h2>
                          </form>
                          
@@ -27,6 +27,6 @@
              </div>
              <?php include("includes/footer.php");?>
 
-             
+      
 </body>
 </html>
---
+++ truenti/framework/registeruser.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once("includes/a_config.php");
+
+// Datos para conectar a la base de datos.
+$nombreServidor = "localhost";
+$nombreUsuario = "root";
+$passwordBaseDeDatos = "";
+$nombreBaseDeDatos = "loscabesa";
+
+// Crear conexión con la base de datos.
+$conn = new mysqli($nombreServidor, $nombreUsuario, $passwordBaseDeDatos, $nombreBaseDeDatos);
+ 
+// Validar la conexión de base de datos.
+if ($conn ->connect_error) {
+  die("Connection failed: " . $conn ->connect_error);
+}
+
+      if(isset($_POST['enviar'])) { 
+         if($_POST['usuario'] == '' or $_POST['password'] == '' or $_POST['correo'] == '') { 
+             echo 'Por favor llene todos los campos.'; 
+         } else { 
+             $sql = 'SELECT * FROM usuario'; 
+             $rec = mysql_query($sql); 
+             $verificar_usuario = 0; 
+             while($result = mysql_fetch_object($rec)) { 
+                 if($result->usuario == $_POST['usuario']) { 
+                     $verificar_usuario = 1; 
+                 } 
+             } 
+             if($verificar_usuario) { 
+                     $usuario = $_POST['usuario']; 
+                     $password = $_POST['password']; 
+                     $correo = $_POST['correo'];
+                     $sql = "INSERT INTO usuario (username,password,correo_electronico) VALUES ('$usuario','$password', '$correo')"; 
+                     mysql_query($sql); 
+                     header("Location: index.php");
+                 } 
+              else {
+                 echo 'Este usuario ya ha sido registrado anteriormente.'; 
+             } 
+         } 
+     } 
+
+
+      ?>
