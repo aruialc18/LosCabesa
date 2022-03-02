88c5f719d125b3828564f959e61aa3d8d1396f27
Ablarom28 <ablarom28@iesmarquesdecomares.org>
2022-03-02 13:46:00 +0100

    add cosas

--- truenti/framework/index.php
+++ truenti/framework/index.php
@@ -16,13 +16,15 @@ include("includes/cookies.php");
             <div class=" truenti container-fluid page-section  " >
                 <div class="row">
                      <div class="col-sm-6">
-                        <img class=" logo-login img-fluid" src="/assets/truenti/rayo2.png">
+                        <img alt="imagetruenti" class=" logo-login img-fluid" src="/assets/truenti/rayo2.png">
                      </div>
                     <div class="col-sm-6">
                        <h1>Inicio de Sesión</h1>
                         <form  method="POST" action="login.php" class="form-login"  >
-                            <input type="text" name="user" class="form-control-sm" placeholder="Usuario"><br><br>
-                            <input type="password" name="pass" class="form-control-sm" placeholder="Contraseña"><br><br>
+                           <label class="sr-only" for="user">Usuario</label>
+                            <input type="text" id="user" name="user" class="form-control-sm" placeholder="Usuario"><br><br>
+                            <label class="sr-only" for="pass">Clave</label>
+                            <input type="password" id="pass" name="pass" class="form-control-sm" placeholder="Contraseña"><br><br>
                             <?php
                             if ($_GET["error"]=="1"){
                                ?>
@@ -34,12 +36,12 @@ include("includes/cookies.php");
                             <?php
                            
                             //Create a URL to obtain user authorization
-                            $login_button = '<a href="'.$google_client->createAuthUrl().'"><img src="assets/img/sign-in-with-google.png" class="googlebtn"/></a>';
+                            $login_button = '<a href="'.$google_client->createAuthUrl().'"><img alt="googlelogin" src="assets/img/sign-in-with-google.png" class="googlebtn"/></a>';
                             echo $login_button;
                            ?>
                             
                             
-                           <br> <h>¿Aún no tienes cuenta? <a href="register.php">Regístrate</a></h2>
+                           <br> <h>¿Aún no tienes cuenta? <a class="registrate" href="register.php">Regístrate</a></h2>
                          </form>
                          
                      </div>
