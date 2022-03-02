a0e75100687381ccecbfa56cf1e8a7d6f7919083
cristobalguillen47 <cguibar09@iesmarquesdecomares.org>
2022-03-02 13:53:06 +0100

    cambios errores2

--- truenti/framework/home.php
+++ truenti/framework/home.php
@@ -78,11 +78,12 @@ include("includes/a_config.php");?>
                 </div>
             </div>
                 <div class="row">
-
+                
                     <div class="col-md-5 text-center">
-                    <label class="sr-only" for="like">like</label>
+                    
                         <?php echo "".$reg['likes'];?>
-                        <a class="like" href="megusta.php?n=si&id=<?php echo $reg['id'];?>">
+                        <a id="like" class="like" href="megusta.php?n=si&id=<?php echo $reg['id'];?>">
+                        <label class="sr-only" for="like">like</label>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                         </svg>
@@ -90,7 +91,8 @@ include("includes/a_config.php");?>
                     </div>
                     <div class="col-md-5 text-center">
                     <?php echo "".$reg['dislikes'];?>
-                    <a class="like" href="megusta.php?n=no&id=<?php echo $reg['id'];?>">
+                    <a id="like" class="like" href="megusta.php?n=no&id=<?php echo $reg['id'];?>">
+                    <label class="sr-only" for="like">like</label>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heartbreak-fill" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M8.931.586 7 3l1.5 4-2 3L8 15C22.534 5.396 13.757-2.21 8.931.586ZM7.358.77 5.5 3 7 7l-1.5 3 1.815 4.537C-6.533 4.96 2.685-2.467 7.358.77Z"/>
                         </svg>
